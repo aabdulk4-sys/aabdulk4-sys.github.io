eadd8d5e82e19970bc39b6c287eaff4151ea7944
aabdulk4-sys <aabdulk4@charlotte.edu>
2025-12-07 20:11:42 -0500

    update

--- itis3135-react/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <section>
-        <h2>Home</h2>
-        <p>Welcome to my React version of the ITIS3135 site.</p>
-      </section>
-    );
-  }
-  
